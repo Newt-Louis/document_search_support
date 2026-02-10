e0acf43145e99914e98c08bac3f0d9174de0d5ac
newt-louis <lengoc@thiso.vn>
2026-02-10 13:09:44 +0700

    fix download.multilingual.py

--- scripts/download.multilingual.py
+++ scripts/download.multilingual.py
@@ -4,7 +4,7 @@ import os
 # Đổi model sang E5-Large
 MODEL_NAME = "intfloat/multilingual-e5-large"
 # CACHE_DIR = "./data/cache/multilingual-e5-large"
-CACHE_DIR = os.getenv("CACHE_DIR", "./data/cache")
+CACHE_DIR = os.getenv("CACHE_DIR", "./data/cache") + "/multilingual-e5-large"
 
 print(f"⏳ Đang tải model {MODEL_NAME} về thư mục: {os.path.abspath(CACHE_DIR)}...")
 
